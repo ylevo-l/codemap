a9fa6363cf8ac789540ac352eb5720791803245a
lovely <78192693+ylevo-l@users.noreply.github.com>
2024-12-31 21:21:46 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='codemap',
-    version='1.3.0',
+    version='1.4.0',
     description='Scrollable, interactive ASCII tree view of a directory.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
